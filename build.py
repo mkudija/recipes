fa3a2a119fac7a66a888023f2a37ff11a6811248
Matthew Kudija <m.kudija@gmail.com>
2018-03-29 07:57:00 -0400

    incremental save
    article output and theme copy working

--- build.py
+++ build.py
@@ -8,11 +8,11 @@ from pathlib import Path
 from config import *
 
 # --------------------------------------------------------------------------------------------------------
-def delete_folder_contents(src):
-    print('Deleting files...\n')
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        #print('\t{}                  '.format(s))#, end='\r')
+def delete_folder_contents(dst):
+    print('Deleting {}...'.format(dst))
+    for item in os.listdir(dst):
+        s = os.path.join(dst, item)
+        # print('\t{}                  '.format(s))#, end='\r')
         if os.path.isdir(s):
             shutil.rmtree(s)
         else:
@@ -21,60 +21,26 @@ def delete_folder_contents(src):
 
 # --------------------------------------------------------------------------------------------------------
 def copytree(src, dst, symlinks=False, ignore=None):
-    print('Copying files...\n')
+    print('Copying {}'.format(dst))
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
-        #print('\t{}                  '.format(d))#, end='\r')
+        # print('\t{}                  '.format(d))#, end='\r')
         if os.path.isdir(s):
             shutil.copytree(s, d, symlinks, ignore)
         else:
             shutil.copy2(s, d)
  
 
+def convert_md_to_html(article, pathOutput):
+    """Converts markdown (.md) files to .html files.
 
-
-
-
-def convert_md_to_html(pathArticles):
-    # import pandas as pd
-    import markdown2
-    from markdown2 import markdown_path
-
-    def build(shortName, title, author, ISBN, yearPublished, yearRead):
-        with open('_head.html') as f:
-            head = [x.strip('\n,') for x in f]
-       
-        with open('_foot.html') as f:
-            foot = [x.strip('\n,') for x in f]
-        
-        head = [x.strip() for x in head]    
-        foot = [x.strip() for x in foot]
-
-        for line in range(len(head)):
-            head[line] = head[line].replace('#SHORTNAME#',shortName)
-            head[line] = head[line].replace('#TITLE#',title)
-            head[line] = head[line].replace('#AUTHOR#',author)
-            head[line] = head[line].replace('#ISBN#',ISBN)
-            head[line] = head[line].replace('#YEARPUBLISHED#',yearPublished)
-            head[line] = head[line].replace('#YEARREAD#',yearRead)
-                
-        with open(shortName+'.md') as f:
-            md = [x.strip('') for x in f]
-        
-        mdString = ''.join(md)
-        content = markdown2.markdown(mdString, extras=['footnotes','smarty-pants','cuddled-lists','target-blank-links','tables','header-ids','break-on-newline'])
-        content = [content]
-        doc = head+content+foot
-
-        with open('../'+shortName+'.html', mode='wt', encoding='utf-8') as myfile:
-            for lines in doc:
-                myfile.write(''.join(lines))
-                myfile.write('\n')
-        print('\tSaved {}.html'.format(shortName))
+    :param article: pathlib path to article .md file
+    :param metadata: dictionary of file metadata
+    """
 
     def get_metadata(article):
-        """Gets metadata from article.md header.
+        """Gets metadata from article.md templateArticleer.
 
         :param article: pathlib paths to each article
         """
@@ -84,34 +50,64 @@ def convert_md_to_html(pathArticles):
         with open(article) as f:  
             for line, content in enumerate(f):
                 if content.startswith('Title: '):
-                    metadata['title'] = content.replace('Title: ','')
+                    metadata['title'] = content.replace('Title: ','').replace('\n','')
                 if content.startswith('date: '):
-                    metadata['date'] = content.replace('date: ','')
+                    metadata['date'] = content.replace('date: ','').replace('\n','')
                 if content.startswith('updated: '):
-                    metadata['updated'] = content.replace('updated: ','')
+                    metadata['updated'] = content.replace('updated: ','').replace('\n','')
                 if content.startswith('comments: '):
-                    metadata['comments'] = content.replace('comments: ','')
+                    metadata['comments'] = content.replace('comments: ','').replace('\n','')
                 if content.startswith('slug: '):
-                    metadata['slug'] = content.replace('slug: ','')
+                    metadata['slug'] = content.replace('slug: ','').replace('\n','')
                 if content.startswith('tags: '):
                     tags = content.replace('tags: ','').replace('\n','').split(',')
                     tags = [x.lstrip(' ') for x in tags]
                     metadata['tags'] = tags
                 if content.startswith('prepTime: '):
-                    metadata['prepTime'] = content.replace('prepTime: ','')
+                    metadata['prepTime'] = content.replace('prepTime: ','').replace('\n','')
                 if content.startswith('cookTime: '):
-                    metadata['cookTime'] = content.replace('cookTime: ','')
+                    metadata['cookTime'] = content.replace('cookTime: ','').replace('\n','')
                 if content.startswith('image: '):
-                    metadata['image'] = content.replace('image: ','')
+                    metadata['image'] = content.replace('image: ','').replace('\n','')
 
-        print(metadata)
         return metadata
 
 
-    articles = list(pathArticles.glob('*.md'))
+    metadata = get_metadata(article)
 
-    for article in articles:
-        get_metadata(article)
+    import markdown2
+    from markdown2 import markdown_path
+    with open('theme/article.html') as f:
+        templateArticle = [x.strip('\n,') for x in f]
+           
+    templateArticle = [x.strip() for x in templateArticle]    
+
+    for line in range(len(templateArticle)):
+        templateArticle[line] = templateArticle[line].replace('#TITLE#', metadata['title'])
+        templateArticle[line] = templateArticle[line].replace('#COOKTIME#', metadata['cookTime'])
+        templateArticle[line] = templateArticle[line].replace('#PREPTIME', metadata['prepTime'])
+            
+    with open(article) as f:
+        md = [x.strip('') for x in f]
+    
+    mdString = ''.join(md)
+    content = markdown2.markdown(mdString, extras=['footnotes','smarty-pants','cuddled-lists','target-blank-links','tables','templateArticleer-ids','break-on-newline'])
+    content = [content]
+
+    templateArticle[templateArticle.index('#BODY#')] = content
+
+    pathOutput = pathOutput/Path('articles/'+metadata['slug']+'.html')
+    # pathOutput = pathOutput/Path(metadata['slug']+'.html')
+
+    with open(pathOutput, mode='wt', encoding='utf-8') as myfile:
+        for lines in templateArticle:
+            myfile.write(''.join(lines))
+            myfile.write('\n')
+    print('\tSaved {}'.format(pathOutput))
+
+
+
+    
 
 	
 
@@ -131,10 +127,26 @@ if __name__ == "__main__":
     # delete_folder_contents(dst)
     # copytree(src, dst, symlinks=False, ignore=None)
 
+
+    # paths
+    pathOutput = Path(OUTPUT_PATH)
+
+    # get article paths
     pathArticles = Path(PATH+'/articles')
+    articles = list(pathArticles.glob('*.md'))
+
+    # output articles
+    for article in articles:
+        convert_md_to_html(article, pathOutput)
 
 
-    convert_md_to_html(pathArticles)
+    # output index
+
+    # output assets
+    src = Path('theme/assets')
+    dst = pathOutput/'assets'
+    delete_folder_contents(dst)
+    copytree(src, dst, symlinks=False, ignore=None)
 
 
 
